3ddba35234814bb415e6cf8fad220a9896b76806
rodolfoHOk <rodolfo.okino@gmail.com>
2021-04-22 16:32:37 -0300

    head das paginas no _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,14 @@
 import { Provider } from 'next-auth/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../styles/globals';
-import Head from 'next/head';
 import { theme } from '../src/theme';
 import Layout from '../src/components/layout';
 import { AppProps } from 'next/app';
 
-/* Obs: O componente Head adiciona a tag head na página html este é útil para
- * carregarmos as fontes (do texto) externas ao nosso aplicativo. Para após isso
- * conseguimos carregá-las no GlobalStyle que é onde estão nossos estilos css global.
- */
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Despensa</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet"/>
-      </Head>
       <Provider session={pageProps.session}>
         <ThemeProvider theme={theme}>
           <GlobalStyle />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,14 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-/*
- * Necessário para não quebrar os estilos, estilos do styled-components
- * dos elementos aninhados não renderizam, somente renderizam os estilos globais.
- * Para desolver isso temos que renderizar os estilos do lado do servidor e injetamos
- * eles no documento.
- */
 export default class MyDocument extends Document {
-  
+
+  /*
+   * Necessário para não quebrar os estilos, estilos do styled-components
+   * dos elementos aninhados não renderizam, somente renderizam os estilos globais.
+   * Para resolver isso temos que renderizar os estilos do lado do servidor e injetamos
+   * eles no documento.
+   */
   static async getInitialProps(ctx){
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -32,5 +32,26 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  /*
+   * Para inserir o cabeçalho em todas as páginas da aplicação sem ficar recarregando
+   * as informações toda requisição de nova página. Usamos esta maneira para carregar
+   * as fontes do google por exemplo.
+   */
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Despensa</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet"/>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
